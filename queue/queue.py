84b60f2133f9d75ed478f49d263807339a6c9f72
sagdish <forsi1@yahoo.com>
2020-05-26 17:22:45 -0700

    queue implementation using DLL

--- queue/queue.py
+++ queue/queue.py
@@ -13,21 +13,29 @@ return elements in First In First Out order.
 Stretch: What if you could only use instances of your Stack class to implement the Queue?
          What would that look like? How many Stacks would you need? Try it!
 """
+import sys
+sys.path.append('../doubly_linked_list')
+print(sys.path)
+from doubly_linked_list import DoublyLinkedList
+
 class Queue:
     def __init__(self):
         self.size = 0
-        self.storage = []
+        # self.storage = []
+        self.storage = DoublyLinkedList()
     
     def __len__(self):
         return self.size
 
     def enqueue(self, value):
         self.size += 1
-        return self.storage.append(value)
+        # return self.storage.append(value)
+        return self.storage.add_to_tail(value)
 
     def dequeue(self):
         if self.size > 0:
-            self.size =- 1
-            return self.storage.pop(0)
+            self.size -= 1
+            # return self.storage.pop(0)
+            return self.storage.remove_from_head()
         else:
             print('nothing to remove')
